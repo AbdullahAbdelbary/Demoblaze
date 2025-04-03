1c84bcd855ae7ba4f2b1c570028b3f7652c597aa
Abdullah Abdelbary <91626688+AbdullahAbdelbary@users.noreply.github.com>
2025-04-04 00:19:22 +0200

    Update TestScripts.java

--- src/test/java/TestScripts.java
+++ src/test/java/TestScripts.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 
 
 public class TestScripts extends BaseTest{
-/*
+    
     //---------------------- Home Page: test logo & website name ----------------------
     @Test(priority = 1)
     public void testLogoAndWebsiteName() throws InterruptedException {
@@ -17,9 +17,9 @@ public class TestScripts extends BaseTest{
     public void testHomeButton() throws InterruptedException {
         homePage.homeButtonValidation();
     }
-*/
+
     //----------------------------------------------- Contact ----------------------------------------------
-    /*@DataProvider(name = "ContactData")
+    @DataProvider(name = "ContactData")
     public Object[][] getContactData() throws IOException {
         String filePath = "D:\\Testing\\Technical\\Java\\Projects\\GraduationProject3\\demoBlaseGraduation.xlsx";
         return ExcelUtils.readExcelData(filePath, "Sheet3");
@@ -28,7 +28,7 @@ public class TestScripts extends BaseTest{
     @Test(priority = 3, dataProvider = "ContactData")
     public void testContact(String contactEmail, String contactName, String message, String testType) throws InterruptedException {
         contactPage.contactValidation(contactEmail, contactName, message, testType);
-    }*/
+    }
 
     @Test(priority = 4)
     public void testContactClosingViaXsign() throws InterruptedException {
@@ -40,10 +40,10 @@ public class TestScripts extends BaseTest{
         contactPage.closingValidation("close");
     }
 
-    /*@Test(priority = 6)
+    @Test(priority = 6)
     public void testContactClickingOnSendMessageButtonWithEmptyData() throws InterruptedException {
         contactPage.clickingOnSendMessageButtonWithEmptyDataValidation();
-    }*/
+    }
 /*
     //----------------------------------------------- Aboutus ----------------------------------------------
     @Test(priority = 7)
